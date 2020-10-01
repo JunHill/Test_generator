9c06b8e535466ce28a9ec7f80646ac04dd7b62b1
JunHill <18520750@gm.uit.edu.vn>
2020-10-01 07:39:24 +0700

    Update

--- BOT/compare.py
+++ BOT/compare.py
@@ -2,8 +2,8 @@ import random
 import string
 from math import floor
 
-from solution1 import solution
-from 
+from solution2 import solution
+
 # set a fixed random seed to re-create the same input file in the future 
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
@@ -26,7 +26,7 @@ PERCENTAGE["a"] = [0.00001, 0.0001, 0.001, 0.01, 1]
 current_test = 0
 last_n = last_a = 0
 
-
+file = open("out.txt", 'r')
 for p, p_n, p_a in zip(PERCENTAGE["test_set"], PERCENTAGE["n"], PERCENTAGE["a"]):
 	end = floor(p * TEST_NUM)
 	for i in range(current_test, end):
@@ -39,19 +39,15 @@ for p, p_n, p_a in zip(PERCENTAGE["test_set"], PERCENTAGE["n"], PERCENTAGE["a"])
 		for k in range(0, N):
 			Problem["sequence"].append(random.randint(-p_a * CONSTRAINTS["a"], p_a * CONSTRAINTS["a"]))
 
-		# Write problem to file inp.txt
-		inp = open("inp.txt", 'a')
-		for item in Problem["sequence"]:
-			inp.write("%s " % item)
-		inp.write("\n")
-		inp.close()
-
-		# Write solution to file out.txt
-		out = open("out.txt", 'a')
-		out.write(str(solution(Problem)) + "\n")
-		out.close()
-
-		print(f"TEST {i} CREATED!")
+		res = str(solution(Problem))
+		line = file.readline()
+		if not line:
+			break;
+		try:
+			assert res+"\n" == line
+			print(f"Passed test {i}")
+		except:
+			print(f"Failed Test {i}, Expect {line} , Got {res}")
 
 	last_n = floor(p_n * CONSTRAINTS["n"])
 	last_a = floor(p_a * CONSTRAINTS["a"])
